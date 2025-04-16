3aff02c752b8c9f7be1c37c572eea80d65375ce4
PENUMAKA RUPAS BABU <107531957+RUPAS13788@users.noreply.github.com>
2025-04-17 01:51:37 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,74 +3,69 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate software developer with a strong computer science background and hands-on web development experience. I create efficient, user-friendly solutions, leveraging logical skills and innovation. Committed to continuous learning and staying current with industry trends, I am eager to grow professionally and make a meaningful impact in a dynamic software firm.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and full stack developer with a passion for creating efficient and user-friendly web applications. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions.  I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
-  {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
+  
   {
     year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
+    role: "Robotic Process Automation Virtual Internship ",
+    company: "Blue Prism university ",
     description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    technologies: [],
   },
   {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
+    year: "2023 - 2024",
+    role: "Master Data Management for Beginnersr",
+    company: "TCSION",
+    description: ` The core responsibilities of an MDM specialist include developing and implementing data management strategies, ensuring data accuracy, defining data standards, and overseeing data governance policies.`,
+    technologies: [],
+  },
+  {
+    year: "2024 - 2025",
+    role: "Accenture North America Data Analytics and Visualization Job Simulation on Forage",
     company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+    description: `Completed a simulation focused on advising a hypothetical social media client as a Data Analyst at Accenture Cleaned, modelled and analyzed 7 datasets to uncover insights into content trends to inform strategic decisions Prepared a PowerPoint deck and video presentation to communicate key insights for the client and internal stakeholders`,
+    technologies: [],
   },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
-];
+  
+]; 
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Project Tittle : Digital Panchayath",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "A fully functional e-commerce website with features like his project encompasses the establishment of a robust digital infrastructure, implementation of an efficient e-governance platform, and provision of citizen services through online channels.",
+    technologies: ["HTML", "CSS", "MYSQL", "PHP", "XAMP"],
   },
   {
-    title: "Task Management App",
+    title: "Project Tittle : Placement Hub",
     image: project2,
     description:
       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+    technologies: ["HTML", "CSS", "MYSQL", "PHP", "XAMP"],
   },
   {
-    title: "Portfolio Website",
+    title: "Project Tittle : Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
     technologies: ["HTML", "CSS", "React", "Bootstrap"],
   },
   {
-    title: "Blogging Platform",
+    title: "Bookstore Web Application",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "Designed a user-friendly UI with React and Tailwind CSS, improving navigation for 500+ users and enhancing the overall experience.Developed  secure authentication features and implemented React Router for seamless single-page application navigation.",
+    technologies: ["React", "Tailwind CSS", "MongoDB", "Express.js", "Node.js"],
   },
 ];
 
 export const CONTACT = {
-  address: "Prathipadu , west godavari , Andhra pradesh , India ",
+  address: "Prathipadu , West godavari, Andhra pradesh , India ",
   phoneNo: "8897193242 ",
   email: "rupas092003@gmail.com",
 };
