6731663fd4de74ec1703fc17971721ae776b4265
PENUMAKA RUPAS BABU <107531957+RUPAS13788@users.noreply.github.com>
2025-07-13 20:07:32 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,9 @@ import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
   return <nav className=" mb-20 flex items-center justify-between py-6">
-    <div className="flex flex-shrink-0 item-center">
+{/*     <div className="flex flex-shrink-0 item-center">
         <img  className= "mx-2 w-10" src={logo} alt="logo" />
-    </div>
+    </div> */}
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
     <a href="https://www.linkedin.com/in/p-rupas-babu/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
     <a href="https://github.com/RUPAS13788" target="_blank"rel="noopener noreferrer">   <FaGithub /> </a>
